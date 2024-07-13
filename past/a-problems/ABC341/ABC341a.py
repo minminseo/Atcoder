194906fc160f42f6daef5d30415a8fdb6945665e
Masahiro Yamamoto <cmvng00@gmail.com>
2024-07-13 15:12:37 +0900

    a

---
+++ past/a-problems/ABC341/ABC341a.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+for i in range(1, N*2+2):
+    if i % 2 != 0:
+        print(1, end="")
+    else:
+        print(0, end="")
+        
+        
---
+++ past/a-problems/ABC341/ABC341a_2.py
@@ -0,0 +1,13 @@
+"""
+別解
+"""
+
+n=int(input())
+for i in range(n):
+    print("10",end='')
+print("1")
+
+# or
+
+n=int(input())
+print("10"*n+"1")
