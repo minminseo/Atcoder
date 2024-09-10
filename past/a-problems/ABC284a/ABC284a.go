2e81cacb2e4d65414dfde097ca88c1e589783b13
Masahiro Yamamoto <cmvng00@gmail.com>
2024-09-10 17:28:01 +0900

    a 昨日の分。Scanner慣れ

--- past/a-problems/ABC284a/ABC284a.go
+++ past/a-problems/ABC284a/ABC284a.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	s := make([]string, n)
+
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		s[i] = scanner.Text()
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		fmt.Println(s[i])
+	}
 }
