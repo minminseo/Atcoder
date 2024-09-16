6183d58ee783b9419014bef8ccb368ff6d24a308
Masahiro Yamamoto <cmvng00@gmail.com>
2024-09-16 23:54:46 +0900

    a

---
+++ past/a-problems/ABC266a/ABC266a.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	s := scanner.Text()
+
+	fmt.Println(string(s[len(s)/2]))
+}
