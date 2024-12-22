de79a31c44e3519e5ccb594eeec3955464051b84
Masahiro Yamamoto <cmvng00@gmail.com>
2024-12-22 23:36:15 +0900

    b

---
+++ past/b-problems/ABC214b/ABC214b.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	if n <= 125 {
+		fmt.Println(4)
+	} else if n <= 211 {
+		fmt.Println(6)
+	} else if n <= 214 {
+		fmt.Println(8)
+	}
+}
