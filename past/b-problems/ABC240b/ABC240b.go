e642baf1d7447692bc9aaa680fe6240c7ec22457
Masahiro Yamamoto <cmvng00@gmail.com>
2025-01-14 23:41:27 +0900

    b

---
+++ past/b-problems/ABC240b/ABC240b.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	inputs := strings.Split(sc.Text(), " ")
+
+	Map := make(map[string]string, len(inputs))
+	count := 0
+
+	for i, c := range inputs {
+		if Map[inputs[i]] == "" {
+			Map[inputs[i]] = c
+			count = count + 1
+		} else {
+			count = count
+		}
+	}
+
+	fmt.Println(count)
+
+}
