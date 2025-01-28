392f99369d5fdaeff748c4aa206b7e38fa25bb51
Masahiro Yamamoto <cmvng00@gmail.com>
2025-01-28 16:06:15 +0900

    a

---
+++ past/a-problems/ABC388a/ABC388a.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s := scanner.Text()
+
+	fmt.Println(s[0:1] + "UPC")
+}
